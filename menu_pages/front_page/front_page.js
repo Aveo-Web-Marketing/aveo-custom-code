a80912b8f9c70e245f2a5f98518a07ca84e4a4ae
madsnbkristensen <112545351+madsnbkristensen@users.noreply.github.com>
2024-04-10 15:00:22 +0200

    front_page update

--- menu_pages/front_page/front_page.js
+++ menu_pages/front_page/front_page.js
@@ -37,3 +37,55 @@ async function snippet_activation_status_update(snippet_id, activation_status) {
     );
 
 }
+
+// Filter snippets by type
+jQuery(function($) {
+    $('.snippet-type-filter-wrap div').on('click', function() {
+        let category_id = $(this).data('category');
+        // Add active class to the clicked item and remove from siblings
+        $(this).addClass('active').siblings().removeClass('active');
+
+        // Loop through each snippet row
+        $('.aveo-single-snippet').each(function() {
+            // Check if we are showing all snippets or if the snippet's type matches the selected category
+            if (category_id === 'all_snippets' || $(this).data('snippet_type') === category_id) {
+                $(this).show(); // Show the row if it matches the filter
+            } else {
+                $(this).hide(); // Hide the row if it does not match the filter
+            }
+        });
+    });
+});
+
+// Function to hide / show the import wrapper. Using jQuery animation
+jQuery(function($) {
+    $('.import-snippet-button').on('click', function() {
+        $('.import-snippet-animate').slideToggle();
+    });
+}); 
+
+
+// Function to create a drag and drop area for the import of snippets
+$(document).ready(function() {
+    // Highlight the label on drag over
+    $('#file-input-label').on('dragover', function(e) {
+        e.preventDefault();
+        $(this).addClass('dragover');
+    });
+
+    // Remove highlight from the label on drag leave
+    $('#file-input-label').on('dragleave', function(e) {
+        e.preventDefault();
+        $(this).removeClass('dragover');
+    });
+
+    // Handle file drop
+    $('#file-input-label').on('drop', function(e) {
+        e.preventDefault();
+        var files = e.originalEvent.dataTransfer.files; // Get the files that were dropped
+        $('#import-snippet-file').prop('files', files); // Assign dropped files to file input
+
+        // Optional: Update text
+        $('.file-input-text').text('File Selected');
+    });
+});
--- menu_pages/front_page/front_page.php
+++ menu_pages/front_page/front_page.php
@@ -48,7 +48,7 @@ function aveo_custom_code_front_page() {
         }
 
         $table_row_html .= '
-            <tr>
+            <tr class="aveo-single-snippet" data-snippet_type="'. $snippet->type .'" data-snippet_id="' . $snippet->id . '">
                 <td><input type="checkbox" class="snippet-checkbox" data-snippet_id="' . $snippet->id . '"></td>
                 <td>  
                     <span class="snippet-activate-switch-con">
@@ -58,14 +58,14 @@ function aveo_custom_code_front_page() {
                         </label>
                     </span> 
                 </td>
-                <td>
-                <a href="'. $snippet_edit_url .'"> ' . $snippet->name . '</a>
-                <div class="snippet-actions">
-                    <a href="' . $snippet_edit_url . '">Edit</a>
-                    <a href="#" class="clone-snippet" data-snippet_id="' . $snippet->id . '">Clone</a>
-                    <a href="#" class="export-snippet" data-snippet_id="' . $snippet->id . '">Export</a>
-                    <a href="#" class="delete-snippet" data-snippet_id="' . $snippet->id . '">Delete</a>
-                </div>
+                <td class="snippet-name-td">
+                    <a class="snippet-name" href="'. $snippet_edit_url .'"> ' . $snippet->name . '</a>
+                    <div class="snippet-actions">
+                        <a href="' . $snippet_edit_url . '">Edit</a>
+                        <a href="#" class="clone-snippet" data-snippet_id="' . $snippet->id . '">Clone</a>
+                        <a href="#" class="export-snippet" data-snippet_id="' . $snippet->id . '">Export</a>
+                        <a href="#" class="delete-snippet" data-snippet_id="' . $snippet->id . '">Delete</a>
+                    </div>
                 </td>
                 <td>' . $snippet->type . '</td>
                 <td>' . $snippet->description . '</td>
@@ -88,43 +88,53 @@ function aveo_custom_code_front_page() {
             </div>
         </div>
 
-        <div class="import-snippet-wrap">
-            <p>Upload from this device</p>
-            <div>
-                <input type="file" id="import-snippet-file"> 
-                <div class="aveo-custom-code-snippet-condition-wrap">
-                    <div>
-                        <label for="Snippet type">Document Type</label>
-                        <select class="aveo_snippet_type" name="aveo_snippet_type">
-                            <option value="php">PHP</option>
-                            <option value="css">CSS</option>
-                            <option value="js">JavaScript</option>
-                        </select>
-                        <span>This should match the code you write</span>
+        <div class="import-snippet-animate">
+            <div class="import-snippet-wrap">
+                <p>Upload from this device</p>
+                <div class="import-snippet-content">
+                    <div class="import-snippet-file-wrap">
+                        <input type="file" id="import-snippet-file" style="display:none;">
+                        <label for="import-snippet-file" id="file-input-label">
+                            <div class="file-input-content">
+                                <span class="dashicons dashicons-plus custom-plus-icon"></span>
+                                <span class="file-input-text">Click or drag file here</span>
+                            </div>
+                        </label>
                     </div>
-                    <div>
-                        <label for="Snippet Condition">Where to run code</label>
-                        <select name="aveo_snippet_condition">
-                            <option value="everywhere">Everywhere</option>
-                            <option value="only_frontend">Only in the Frontend</option>
-                            <option value="only_backend">Only in the WP backend</option>
-                        </select>
+                    <div class="aveo-custom-code-snippet-condition-wrap">
+                        <div>
+                            <label for="Snippet type">Document Type</label>
+                            <select class="aveo_snippet_type" name="aveo_snippet_type">
+                                <option value="php">PHP</option>
+                                <option value="css">CSS</option>
+                                <option value="js">JavaScript</option>
+                            </select>
+                            <span>This should match the code you write</span>
+                        </div>
+                        <div>
+                            <label for="Snippet Condition">Where to run code</label>
+                            <select name="aveo_snippet_condition">
+                                <option value="everywhere">Everywhere</option>
+                                <option value="only_frontend">Only in the Frontend</option>
+                                <option value="only_backend">Only in the WP backend</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="Snippet activation">Activate snippet on save</label>
+                            <input type="checkbox" name="aveo_snippet_active" value="1" checked>
+                        </div>
+                        <input type="submit" name="aveo_submit_snippet" value="Save Snippet" class="button button-primary">
                     </div>
-                    <div>
-                        <label for="Snippet activation">Activate snippet on save</label>
-                        <input type="checkbox" name="aveo_snippet_active" value="1" checked>
-                    </div>
-                    <input type="submit" name="aveo_submit_snippet" value="Save Snippet" class="button button-primary">
                 </div>
             </div>
         </div>
 
         <div class="snippet-type-filter-wrap">
-            <div class="all-snippets">All your snippets</div>
-            <div class="php-snippets">PHP</div>
-            <div class="js-snippets">JavaScript</div>
-            <div class="css-snippets">CSS</div>
-            <div class="premade-snippets">Premade snippets</div>
+            <div data-category="all_snippets" class="all-snippets active">All your snippets</div>
+            <div data-category="php" class="php-snippets">PHP</div>
+            <div data-category="js" class="js-snippets">JavaScript</div>
+            <div data-category="css" class="css-snippets">CSS</div>
+            <div data-category="premade_snippets" class="premade-snippets">Premade snippets</div>
             <span class="num-of-snippets">' . count($snippets) . ' items</span>
         </div>
 
@@ -132,9 +142,9 @@ function aveo_custom_code_front_page() {
             <thead>
                 <tr>
                     <th><input type="checkbox" id="select-all-snippets"></th>
-                    <th>Activation</th>
-                    <th>Snippet Name</th>
-                    <th>type</th>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Type</th>
                     <th>Description</th>
                     <th>Modified</th>
                     <th>Priority</th>
